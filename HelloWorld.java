a5f9104d7f6317cb876c695d9d0a93a4a9c42be5
Anners <anners@mailzorz.com>
2019-10-28 14:41:09 +0100

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+class HelloWorld
+
+
+{
+	public static void main(String args[])
+	{
+	JOptionPane.showMessageDialog(null, "Hejsan hopsan");
+	System.exit(0);
+	}
+}
